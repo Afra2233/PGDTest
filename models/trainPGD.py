fe9c24f97d8d87df357f20fd1f241488f3d76022
Afra2233 <zhangafra818@gmail.com>
2024-09-21 16:10:39 +0100

    Update trainPGD.py

--- models/trainPGD.py
+++ models/trainPGD.py
@@ -192,6 +192,9 @@ class myLightningModule(LightningModule):
             prompted_images = self.prompter(normalize(X + delta))
             prompt_token = self.add_prompter()
             output= multiGPU_CLIP(self.model, prompted_images, text_tokens)#, prompt_token)
+             '''
+                X.shape[0] 获取这个张量第一个维度的大小。在处理图像数据时，这个维度通常是批次大小（batch size），即批次中包含的图像数量。
+             '''
             label_mask = one_hot_embedding(torch.arange(X.shape(0),device=X.device), output.size(1)) #每个整数标签都被转换为一个全为0且只有一个位置为1的向量
             correct_logit = torch.sum(label_mask * output, dim=1)
             wrong_logit, _ = torch.max((1 - label_mask) * output - 1e4 * label_mask, axis=1)
