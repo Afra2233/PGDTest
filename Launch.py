49baca026c484e625e028ab480c5505f00e449d7
afra818 <60651664+afra818@users.noreply.github.com>
2024-09-23 16:11:03 +0100

    fx

--- Launch.py
+++ Launch.py
@@ -115,13 +115,11 @@ def SlurmRun(trialconfig):
     else:
 
         sub_commands.extend(['#SBATCH -p gpu-medium',
-<<<<<<< HEAD
+
                              'export CONDADIR=/storage/hpc/07/zhang303/conda_envs/torch',                                                     #<-----CHANGE ME
-=======
+
                              #add command to request more memory
                              '#SBATCH --mem=64G',
-                             'export CONDADIR=/storage/hpc/46/manders3/conda4/open-ce',                                                     #<-----CHANGE ME
->>>>>>> upstream/main
                              'export NCCL_SOCKET_IFNAME=enp0s31f6',])
     sub_commands.extend([ '#SBATCH --{}={}\n'.format(cmd, value) for  (cmd, value) in slurm_commands.items()])
     sub_commands.extend([
