31aed753cc67b6b721a33de5bb6a6b9ea6ffa892
afra818 <60651664+afra818@users.noreply.github.com>
2024-09-24 14:39:23 +0100

    fx

--- Launch.py
+++ Launch.py
@@ -120,7 +120,7 @@ def SlurmRun(trialconfig):
 
                              #add command to request more memory
                              '#SBATCH --mem=128G',
-                             'export CONDADIR=/storage/hpc/46/manders3/conda4/open-ce',                                                     #<-----CHANGE ME
+                             'export CONDADIR=/storage/hpc/07/zhang303/conda_envs/torch',                                                     #<-----CHANGE ME
                              'export NCCL_SOCKET_IFNAME=enp0s31f6',])
     sub_commands.extend([ '#SBATCH --{}={}\n'.format(cmd, value) for  (cmd, value) in slurm_commands.items()])
     sub_commands.extend([
