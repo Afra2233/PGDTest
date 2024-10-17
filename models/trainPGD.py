4d8a8fff8cd2156a8214ecacef8ed8959c7d13a1
Afra2233 <zhangafra818@gmail.com>
2024-10-17 18:41:36 +0100

    Update trainPGD.py

--- models/trainPGD.py
+++ models/trainPGD.py
@@ -692,7 +692,7 @@ class myLightningModule(LightningModule):
             new_images = torch.add(X, delta)
             prompted_images = torch.div(torch.sub(new_images, self.mu_img.clone()), self.std_img.clone()) #normalize(new_images) but preserves grad
             print(prompted_images.requires_grad)
-            img_embed=self.model.encode_image(prompted_images.flatten(0,-4))
+            img_embed=self.model.encode_image(prompted_images.flatten(0,-4)).clone()
             img_embed = img_embed / img_embed.norm(dim=-1, keepdim=True)
             scale_text_embed=self.model.encode_text(text_tokens)
             scale_text_embed = scale_text_embed / scale_text_embed.norm(dim=-1, keepdim=True)
