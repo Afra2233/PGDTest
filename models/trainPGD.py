2f0e797f1e016c31949da66c2c38619f5748390e
Afra2233 <zhangafra818@gmail.com>
2024-10-29 23:12:15 +0000

    Update trainPGD.py

--- models/trainPGD.py
+++ models/trainPGD.py
@@ -1131,7 +1131,7 @@ class myLightningModule(LightningModule):
         threshold=50
         EmptyCount=0
         while EmptyCount < 3:
-            sleep(60)
+            time.sleep(60)
             clear=False
             print("save 2")
             for dataset_idx in range(self.test_data_loader_count):
