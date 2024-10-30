a40979997ef11a8f70467ef9973fdc6c41d3adbf
Afra2233 <zhangafra818@gmail.com>
2024-10-30 14:04:20 +0000

    Update DataModule.py

--- DataModule.py
+++ DataModule.py
@@ -179,7 +179,7 @@ Add COCO captions here ...
 
 '''
 class MyDataModule(pl.LightningDataModule):
-    def __init__(self,Cache_dir, dataset: str,batch_size: int,test_batch_size:int=-1, imagenet_root: str="./data", tinyimagenet_root: str="./data",  val_dataset_names: List[str]=None,**kwargs):
+    def __init__(self,Cache_dir, dataset: str,batch_size: int,test_batch_size:int=-1, imagenet_root: str="./data", tinyimagenet_root: str=None,  val_dataset_names: List[str]=None,**kwargs):
         super().__init__()
         self.cache_dir = Cache_dir
         self.imagenet_root = imagenet_root
@@ -535,7 +535,7 @@ class MyDataModule(pl.LightningDataModule):
                         #step four - remove the images folder
                         shutil.rmtree(os.path.join(self.tinyimagenet_root,"tiny-imagenet-200","val",'images'))
                         
-                    val_dataset_dict.update({'tinyImageNet': ImageFolder(os.path.join(self.tinyimagenet_root,'tiny-imagenet-200', 'val'), transform=preprocess224)})
+                    # val_dataset_dict.update({'tinyImageNet': ImageFolder(os.path.join(self.tinyimagenet_root,'tiny-imagenet-200', 'val'), transform=preprocess224)})
 
 
                     # val_dataset_list.append(ImageFolder(
