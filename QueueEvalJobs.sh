e4b3b27fe4f4f9ac888b55667c484bdde696a222
Afra2233 <zhangafra818@gmail.com>
2024-11-06 13:04:00 +0000

    Create QueueEvalJobs.sh

---
+++ QueueEvalJobs.sh
@@ -0,0 +1,20 @@
+DIRECTORY="./slurm_scripts"
+
+
+for folder in "$DIRECTORY"/*; do
+    if [ -d "$folder" ]; then  # Check if it's a directory
+        for file in "$folder"/*; do
+            if [ -f "$file" ]; then  # Check if it's a file
+                echo "Submitting job $file"
+                #if hostname ends in bede.dur.ac.uk
+                if [[ $(hostname) == *".bede.dur.ac.uk" ]]; then
+                    sed -i '/module add opence/d' "$file"
+                    sed -i 's/conda activate $CONDADIR/source activate $CONDADIR/g' filename
+                    echo "Running on bede"
+                fi
+                sbatch "$file"
+                echo "Job $file submitted"
+            fi
+        done
+    fi
+done
