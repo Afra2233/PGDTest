77cc238544f1bd0d36538a272d731885476bfbad
Afra2233 <zhangafra818@gmail.com>
2024-11-07 09:27:04 +0000

    Update demoparse.py

--- demoparse.py
+++ demoparse.py
@@ -88,7 +88,7 @@ class baseparser(HyperOptArgumentParser):
         self.opt_list("--learning_rate", default=5e-4, options=[5e-5,5e-4,1e-5], type=float, tunable=True) #originally 5e-5
         self.opt_list("--batch_size", default=64, options=[64], type=int, tunable=True)
         self.opt_list("--train_eps", default=1/255, options=[1/255,2/255,4/255], type=float, tunable=True)
-        self.opt_list("--train_numsteps", default=5, options=[2,5,10], type=int, tunable=True)
+        self.opt_list("--train_numsteps", default=10, options=[5,10], type=int, tunable=True)
         self.opt_list("--train_stepsize", default=1/255, type=float,options=[1/255,2/255,4/255], tunable=True)
         self.opt_list("--test_eps", default=1/255, options=[1/255,2/255,4/255,8/255], type=float, tunable=False)
         self.opt_list("--test_numsteps", default=5, type=int, tunable=False)
