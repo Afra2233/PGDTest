1ae44250b0601f319431a0e83de866493a7af4e3
Afra2233 <zhangafra818@gmail.com>
2024-11-07 17:10:45 +0000

    Update wandb_result.py

--- wandb_result.py
+++ wandb_result.py
@@ -11,8 +11,11 @@ RUN_ID = "795d73up"
 api = wandb.Api()
 run = api.run(f"{ENTITY}/{PROJECT}/{RUN_ID}")
 
+history = run.history()
+logs = history.filter(regex="test_dirty_batch_acc_alpha_.*")
+for index, row in logs.iterrows():
+    print(row.to_dict())
+# test_results = run.summary.get("test_results")  # 替换为你实际的测试结果字段名称
 
-test_results = run.summary.get("test_results")  # 替换为你实际的测试结果字段名称
 
-
-print("Test Results:", test_results)
+# print("Test Results:", test_results)
