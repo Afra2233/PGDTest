7523987dec9cd4c9c9907943f05feec8b5508cde
Afra2233 <zhangafra818@gmail.com>
2024-11-08 00:06:44 +0000

    Update wandb_result.py

--- wandb_result.py
+++ wandb_result.py
@@ -109,7 +109,7 @@ for index, row in logs_classfar.iterrows():
             )
             if match:
                 dataloader_idx, alpha, epsilon, numsteps = match.groups()
-                data.append({
+                df_classifar.append({
                     "alpha": float(alpha),
                     "epsilon": float(epsilon),
                     "numsteps": int(numsteps),
