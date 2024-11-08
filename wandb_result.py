60fdc4ca3be96663ba953d349dd308f03101b9db
Afra2233 <zhangafra818@gmail.com>
2024-11-08 12:33:55 +0000

    Update wandb_result.py

--- wandb_result.py
+++ wandb_result.py
@@ -94,12 +94,12 @@ plt.savefig('test_acc.png')
 
 
 
-summary_data = run.summary
-logs_yy = {key: value for key, value in summary_data.items() if key.startswith("Test General Classifier on Dirty Features")}
+# summary_data = run.summary
+# logs_yy = {key: value for key, value in summary_data.items() if key.startswith("Test General Classifier on Dirty Features")}
 
-# 打印结果
-for col_name, value in logs_yy.items():
-    print(f"{col_name}: {value}")
+# # 打印结果
+# for col_name, value in logs_yy.items():
+#     print(f"{col_name}: {value}")
 
 
 
@@ -113,83 +113,83 @@ logs_classfar = history.filter(regex="Test General Classifier on Dirty Features.
 #         if pd.notna(value):
 #             print(f"{col_name}: {value}")
 
-# 初始化一个空列表存储提取的数据
-data_classifar = []
+# # 初始化一个空列表存储提取的数据
+# data_classifar = []
 
-# 解析每行日志记录
-# for index, row in logs_classfar.iterrows():
-for col_name, value in logs_yy.items():
-    if pd.notna(value):
-        # 使用正则表达式提取 alpha, epsilon, numsteps 和 dataloader_idx
-        # match = re.match(
-        #     r"test_dirty_batch_acc_alpha_([\d.]+)_epsilon_([\d.]+)_numsteps_(\d+)/dataloader_idx_(\d+)",
-        #     col_name
-        # )
-        match = re.match(
-            r"Test General Classifier on Dirty Features on dataset (\d+) alpha ([\d.]+) epsilon ([\d.]+) step (\d+)", 
-            col_name
-        )
-        if match:
-            dataloader_idx, alpha, epsilon, numsteps = match.groups()
-            data_classifar.append({
-                "alpha": float(alpha),
-                "epsilon": float(epsilon),
-                "numsteps": int(numsteps),
-                "dataloader_idx": int(dataloader_idx),
-                "test_accuracy": value
-            })
+# # 解析每行日志记录
+# # for index, row in logs_classfar.iterrows():
+# for col_name, value in logs_yy.items():
+#     if pd.notna(value):
+#         # 使用正则表达式提取 alpha, epsilon, numsteps 和 dataloader_idx
+#         # match = re.match(
+#         #     r"test_dirty_batch_acc_alpha_([\d.]+)_epsilon_([\d.]+)_numsteps_(\d+)/dataloader_idx_(\d+)",
+#         #     col_name
+#         # )
+#         match = re.match(
+#             r"Test General Classifier on Dirty Features on dataset (\d+) alpha ([\d.]+) epsilon ([\d.]+) step (\d+)", 
+#             col_name
+#         )
+#         if match:
+#             dataloader_idx, alpha, epsilon, numsteps = match.groups()
+#             data_classifar.append({
+#                 "alpha": float(alpha),
+#                 "epsilon": float(epsilon),
+#                 "numsteps": int(numsteps),
+#                 "dataloader_idx": int(dataloader_idx),
+#                 "test_accuracy": value
+#             })
 
-# 将数据转为 DataFrame
-df_classifar = pd.DataFrame(data_classifar)
+# # 将数据转为 DataFrame
+# df_classifar = pd.DataFrame(data_classifar)
 
-# 创建一个新的列，将 (alpha, epsilon) 组合作为字符串
-df_classifar['alpha_epsilon_pair'] = df_classifar.apply(lambda row: f"({round(row['alpha'], 6)}, {round(row['epsilon'], 6)})", axis=1)
+# # 创建一个新的列，将 (alpha, epsilon) 组合作为字符串
+# df_classifar['alpha_epsilon_pair'] = df_classifar.apply(lambda row: f"({round(row['alpha'], 6)}, {round(row['epsilon'], 6)})", axis=1)
 
-# # 映射 dataloader_idx 到数据集名称
-# dataset_mapping = {
-#     0: "cifar10",
-#     1: "cifar100",
-#     2: "STL10",
-#     3: "Food101",
-#     4: "dtd",
-#     5: "fgvc_aircraft",
-#     6: "tinyImageNet"
-# }
+# # # 映射 dataloader_idx 到数据集名称
+# # dataset_mapping = {
+# #     0: "cifar10",
+# #     1: "cifar100",
+# #     2: "STL10",
+# #     3: "Food101",
+# #     4: "dtd",
+# #     5: "fgvc_aircraft",
+# #     6: "tinyImageNet"
+# # }
 
-# # 替换 dataloader_idx 为数据集名称
-df_classifar['dataset'] = df_classifar['dataloader_idx'].map(dataset_mapping)
+# # # 替换 dataloader_idx 为数据集名称
+# df_classifar['dataset'] = df_classifar['dataloader_idx'].map(dataset_mapping)
 
-# 检查数据格式是否正确
-print(df_classifar.head())
+# # 检查数据格式是否正确
+# print(df_classifar.head())
 
-# 设置绘图样式
-plt.figure(figsize=(16, 20))
+# # 设置绘图样式
+# plt.figure(figsize=(16, 20))
 
-# 绘制测试准确率条形图，用颜色表示 dataloader_idx
-g = sns.barplot(
-    data=df_classifar,
-    x="alpha_epsilon_pair",
-    y="test_accuracy",
-    hue="dataset",
-    dodge=True,  # 将不同的 dataloader_idx 在每个 alpha_epsilon_pair 上分开显示
-    ci=None
-    # errorbar=None
-)
+# # 绘制测试准确率条形图，用颜色表示 dataloader_idx
+# g = sns.barplot(
+#     data=df_classifar,
+#     x="alpha_epsilon_pair",
+#     y="test_accuracy",
+#     hue="dataset",
+#     dodge=True,  # 将不同的 dataloader_idx 在每个 alpha_epsilon_pair 上分开显示
+#     ci=None
+#     # errorbar=None
+# )
 
-# 添加图例和标签
-plt.title("Test Accuracy by (Alpha, Epsilon) Pair and Dataset (With Linear prob)")
-plt.xlabel("(Alpha, Epsilon) Pair")
-plt.ylabel("Test Accuracy")
-plt.xticks(rotation=90)
-plt.legend(title="Dataset")
-plt.tight_layout()
-# plt.ylim(50, 100)
-# plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda y, _: f'{y * 100:.0f}'))
-plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda y, _: '{:.0f}'.format(y * 100)))
+# # 添加图例和标签
+# plt.title("Test Accuracy by (Alpha, Epsilon) Pair and Dataset (With Linear prob)")
+# plt.xlabel("(Alpha, Epsilon) Pair")
+# plt.ylabel("Test Accuracy")
+# plt.xticks(rotation=90)
+# plt.legend(title="Dataset")
+# plt.tight_layout()
+# # plt.ylim(50, 100)
+# # plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda y, _: f'{y * 100:.0f}'))
+# plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda y, _: '{:.0f}'.format(y * 100)))
 
-# 显示图表
-plt.show()
-plt.savefig('test_classifiers_acc.png')
+# # 显示图表
+# plt.show()
+# plt.savefig('test_classifiers_acc.png')
 
 # logs_Test_Classifier = history.filter(regex="^Test General Classifier.*")
 
