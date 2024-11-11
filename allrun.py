ffb08b0c079ad9cdfab13a4fa5a4245fc19514f5
Afra2233 <zhangafra818@gmail.com>
2024-11-11 17:39:36 +0000

    Update allrun.py

--- allrun.py
+++ allrun.py
@@ -3,14 +3,14 @@ api = wandb.Api()
 
 ENTITY = "st7ma784"  
 PROJECT = "AllDataPGN"
-runs = api.runs(f"{ENTITY}/{PROJECT}")
+
 
 clusters = {
     1/255: [],
     4/255: [],
     8/255: []
 }
-runs = project.runs()
+runs = api.runs(f"{ENTITY}/{PROJECT}")
 for run in runs:
     alpha = run.config.get("train_stepsize")
     if alpha in clusters:
