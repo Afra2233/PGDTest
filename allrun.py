44ae81797ff1fd8efc4602f254f313fc08c8a52a
Afra2233 <zhangafra818@gmail.com>
2024-11-11 20:02:48 +0000

    Update allrun.py

--- allrun.py
+++ allrun.py
@@ -31,45 +31,33 @@ clusters = {
     2/255: {1/255: [], 2/255: [], 4/255: []},
     4/255: {1/255: [], 2/255: [], 4/255: []}
 }
-average_accuracies = {}
+accuracies = {key: {subkey: [] for subkey in clusters[key]} for key in clusters}
+# average_accuracies = {}
 runs = api.runs(f"{ENTITY}/{PROJECT}")
 for run in runs:
-    train_stepsize = run.config.get("train_stepsize")
+     train_stepsize = run.config.get("train_stepsize")
     train_eps = run.config.get("train_eps")
     
-    # if train_stepsize in clusters:
-    #     if train_eps in clusters[train_stepsize]:
-    #         clusters[train_stepsize][train_eps].append(run)
     if train_stepsize in clusters and train_eps in clusters[train_stepsize]:
-        log_keys = run.history(keys=["_step", "Test General Classifier on Dirty Features on dataset 5 alpha * epsilon * step *"])
-        for log in log_keys:
-            key = "Test General Classifier on Dirty Features on dataset 5 alpha {} epsilon {} step {}".format(run.config.get("dataloader_idx"), train_stepsize, train_eps)
-            if key in log:
-                accuracy = log[key]
-                clusters[train_stepsize][train_eps].append(accuracy)
+        # Fetch logs and filter those relevant to the specified dataset
+        for log in run.scan_history(keys=["_runtime", "_timestamp"]):
+            if "Test General Classifier on Dirty Features on dataset 5" in log:
+                acc_key = "Test General Classifier on Dirty Features on dataset {} alpha {} epsilon {} step {}".format(run.config['DataLoader_idx'], train_eps, run.config['train_stepsize'])
+                if acc_key in log:
+                    accuracies[train_stepsize][train_eps].append(log[acc_key])
 
-# 计算每个分类的平均准确率
-for stepsize, eps_dict in clusters.items():
-    for eps, accuracies in eps_dict.items():
-        if accuracies:  # 确保列表不为空
-            avg_accuracy = sum(accuracies) / len(accuracies)
-            average_accuracies[(stepsize, eps)] = avg_accuracy
+# Calculate average accuracies and prepare data for plotting
+plot_data = {key: np.mean(values) for key, values in accuracies.items() if values}
 
-# 数据准备
-steps_eps = list(average_accuracies.keys())
-accuracies = [average_accuracies[key] for key in steps_eps]
-
-# 绘制曲线图
-plt.figure(figsize=(10, 6))
-for (stepsize, eps), accuracy in zip(steps_eps, accuracies):
-    plt.plot(stepsize, accuracy, 'o-', label=f'stepsize {stepsize}, eps {eps}')
-
-plt.xlabel('Train Stepsize and Epsilon')
-plt.ylabel('Average Accuracy')
-plt.title('Average Accuracy for Different Train Stepsize and Epsilon')
-plt.legend()
+# Plotting
+fig, ax = plt.subplots()
+for stepsize, eps_dict in plot_data.items():
+    ax.plot(list(eps_dict.keys()), list(eps_dict.values()), label=f'stepsize {stepsize}')
+ax.set_xlabel('train_eps')
+ax.set_ylabel('Average Accuracy')
+ax.set_title('Average Accuracy vs. Train Step Size')
+ax.legend()
 plt.show()
-
 # # Print the runs organized by train_stepsize and then by train_eps
 # for stepsize, eps_clusters in clusters.items():
 #     print(f"Runs with train_stepsize {stepsize:.6f}:")
