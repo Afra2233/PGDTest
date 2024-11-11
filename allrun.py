b5dcd1108baad2e127dd04be8c3734833643ac56
Afra2233 <zhangafra818@gmail.com>
2024-11-11 19:47:29 +0000

    Update allrun.py

--- allrun.py
+++ allrun.py
@@ -10,7 +10,7 @@ clusters = {
     2/255: {1/255: [], 2/255: [], 4/255: []},
     4/255: {1/255: [], 2/255: [], 4/255: []}
 }
-
+average_accuracies = {}
 runs = api.runs(f"{ENTITY}/{PROJECT}")
 for run in runs:
     train_stepsize = run.config.get("train_stepsize")
@@ -49,11 +49,11 @@ plt.title('Average Accuracy for Different Train Stepsize and Epsilon')
 plt.legend()
 plt.show()
 
-# Print the runs organized by train_stepsize and then by train_eps
-for stepsize, eps_clusters in clusters.items():
-    print(f"Runs with train_stepsize {stepsize:.6f}:")
-    for eps, runs_in_cluster in eps_clusters.items():
-        print(f"  Eps {eps:.6f}: {len(runs_in_cluster)} runs")
-        for run in runs_in_cluster:
-            print(f"    Run ID: {run.id}, Name: {run.name}")
+# # Print the runs organized by train_stepsize and then by train_eps
+# for stepsize, eps_clusters in clusters.items():
+#     print(f"Runs with train_stepsize {stepsize:.6f}:")
+#     for eps, runs_in_cluster in eps_clusters.items():
+#         print(f"  Eps {eps:.6f}: {len(runs_in_cluster)} runs")
+#         for run in runs_in_cluster:
+#             print(f"    Run ID: {run.id}, Name: {run.name}")
 
