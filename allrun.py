1eab2c17351b762d5dcd979a5912b0a4ff4e4d15
Afra2233 <zhangafra818@gmail.com>
2024-11-12 14:30:51 +0000

    Update allrun.py

--- allrun.py
+++ allrun.py
@@ -127,7 +127,7 @@ fig, ax = plt.subplots(figsize=(10, 10))
 # colors = plt.cm.viridis(np.linspace(0, 1, len(avg_accuracies)))
 # colors = plt.get_cmap('Spectral')(np.linspace(0, 1, len(avg_accuracies)))
 colors = plt.get_cmap('tab20b')(np.linspace(0, 1, len(avg_accuracies)))
-
+np.random.shuffle(colors) 
 
 x_pos = np.arange(1, len(run_ids) + 1)
 # 绘制柱状图
@@ -188,6 +188,7 @@ fig, ax = plt.subplots(figsize=(10, 10))
 # colors = plt.cm.viridis(np.linspace(0, 1, len(dc_avg_accuracies)))
 # colors = plt.get_cmap('Spectral')(np.linspace(0, 1, len(dc_avg_accuracies)))
 colors = plt.get_cmap('tab20b')(np.linspace(0, 1, len(dc_avg_accuracies)))
+np.random.shuffle(colors) 
 
 x_pos = np.arange(1, len(dc_run_ids) + 1)
 # 绘制柱状图
@@ -210,11 +211,17 @@ plt.savefig('DC_Classifier.png')
 x_values = np.array(avg_accuracies)
 y_values = np.array(dc_avg_accuracies)
 
+markers = ['o', 's', 'D', '^', 'p']  # 圆形、方形、菱形、三角形、五边形
+colors = ['blue', 'green', 'purple', 'orange', 'red']
+
 # 创建图形和轴对象
 fig, ax = plt.subplots(figsize=(10, 10))
 
 # 绘制散点图
-scatter = ax.scatter(x_values, y_values, color='blue', marker='o')
+for i in range(len(x_values)):
+    ax.scatter(x_values[i], y_values[i], color=colors[i % len(colors)], marker=markers[i % len(markers)], s=100, label=run_ids[i])
+
+# scatter = ax.scatter(x_values, y_values, color='blue', marker='o')
 
 # 使用 np.polyfit 计算最佳拟合线的斜率和截距
 slope, intercept = np.polyfit(x_values, y_values, 1)
