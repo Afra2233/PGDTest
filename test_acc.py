e0fd85ae66f34e8fad0a17360912617318ed2d1e
Afra2233 <zhangafra818@gmail.com>
2024-11-19 15:28:08 +0000

    Update test_acc.py

--- test_acc.py
+++ test_acc.py
@@ -11,9 +11,7 @@ wandb.login()
 ENTITY = "st7ma784"  
 PROJECT = "AllDataPGN"  
 RUN_ID = "f5jq1b65" 
-# RUN_ID = "olnxxo2t"
-# f5jq1b65 1h2lllig
-# RUN_ID = "as33xopq"
+
 
 
 api = wandb.Api()
@@ -55,11 +53,11 @@ for col_name, value in logs_yy.items():
                  "test_accuracy": value
                 })
 
-# # 解析每行日志记录
+
 # for index, row in logs.iterrows():
 #     for col_name, value in row.items():
 #         if pd.notna(value):
-#             # 使用正则表达式提取 alpha, epsilon, numsteps 和 dataloader_idx
+#            
 #             match = re.match(
 #                 r"test_dirty_batch_acc_alpha_([\d.]+)_epsilon_([\d.]+)_numsteps_(\d+)/dataloader_idx_(\d+)",
 #                 col_name
@@ -75,36 +73,36 @@ for col_name, value in logs_yy.items():
 #                 })
 print(data)
 
-# 将数据转为 DataFrame
+
 df = pd.DataFrame(data)
 
-# # 创建一个新的列，将 (alpha, epsilon) 组合作为字符串
+
 df['alpha_epsilon_pair_long'] = df.apply(lambda row: f"({round(row['alpha'], 6)}, {round(row['epsilon'], 6)})", axis=1)
 df['alpha_epsilon_pair'] = df.apply(lambda row: f"({Fraction(row['alpha']).limit_denominator()}, {Fraction(row['epsilon']).limit_denominator()})", axis=1)
 
 
 
-# 替换 dataloader_idx 为数据集名称
+
 df['dataset'] = df['dataloader_idx'].map(dataset_mapping)
 
-# 检查数据格式是否正确
+
 print(df.head())
 
-# 设置绘图样式
+
 plt.figure(figsize=(14, 14))
 
-# 绘制测试准确率条形图，用颜色表示 dataloader_idx
+
 g = sns.barplot(
     data=df,
     x="alpha_epsilon_pair",
     y="test_accuracy",
     hue="dataset",
-    dodge=True,  # 将不同的 dataloader_idx 在每个 alpha_epsilon_pair 上分开显示
+    dodge=True, 
     # ci=None
     errorbar=None
 )
 
-# 添加图例和标签
+
 # plt.title("Test Accuracy by (Alpha, Epsilon) Pair and Dataset (Without Linear Prob)",fontsize=24)
 plt.xlabel("(Alpha, Epsilon) Pair",fontsize=24)
 plt.ylabel("Test Accuracy",fontsize=24)
@@ -113,6 +111,5 @@ plt.yticks(fontsize=24)
 plt.legend(title="Dataset",fontsize=24)
 plt.tight_layout()
 
-# # 显示图表
 plt.show()
 plt.savefig('test_acc2.png')
