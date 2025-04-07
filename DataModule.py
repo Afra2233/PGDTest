d36b47758dd3a7f8e900aae364a1d7cc402ec833
afra818 <60651664+afra818@users.noreply.github.com>
2025-04-07 15:56:42 +0100

    t

--- DataModule.py
+++ DataModule.py
@@ -19,7 +19,7 @@ from torch.utils.data import Dataset, DataLoader,default_collate
 #import the collate function from pytorch 
 # from torch.utils.data.dataloader import default_collate
 from utils import load_imagenet_folder2name
-from datasets import country211,eurosat,flowers102,oxford_iiit_pet,pcam,stanford_cars,sun397
+from datasets import country211,eurosat,flowers102,oxford_iiit_pet,pcam,stanford_cars,SUN97
 
 
 import os
